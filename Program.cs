2d63004f5e18a23b6459cc5b2af4178dd242f8b0
Anton Gargalyk <gargalyk1994@gmail.com>
2023-03-24 21:28:26 +0900

    two commits

--- Program.cs
+++ Program.cs
@@ -2,8 +2,24 @@
 
 
 Console.Clear();
-using System;
-using static System.Console;
+// using System;
+// using static System.Console;
+Console.WriteLine("Введите массив строк через пробел: ");
+string str = Console.ReadLine();
+string[] array = str.Split(' ');
+//string[] array = new string {"hello" , "2" , "world", "like" , "Geek", "Brains", ":)" };
+string[] arr = GetArray(array);
+Console.WriteLine(String.Join(", ", arr));
 
-Write("Введите кол-во ': ");
-int rows = int.Parse(ReadLine()!);
+string[] GetArray(string[] array)
+{
+    string[] result = new string [array.Length];
+    int size = 3; 
+    if (array.Length < size)
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = array[i];
+    } 
+    return result;
+}
+
